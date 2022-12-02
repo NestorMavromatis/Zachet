e050dd6da0d3e14a86d6b5dcca1de5fbd517a7d4
n3m3m <klo4ke@gmail.com>
2022-12-02 15:59:06 +0300

    Digits mf

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hiu, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+Line=input(" "). split()
+cnt = 0
+for i, s in enumerate(line):
+    if s.isdigit():
+        cnt+= len(s)
+if cnt == 0:
+    print("Нема цифор")
+else:
+    print("", cnt)

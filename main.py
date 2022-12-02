1720d3f2f6648972cc948d358d836ed4cca52b4f
n3m3m <klo4ke@gmail.com>
2022-12-02 16:33:12 +0300

    Digits mf new branch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-Line=input(" "). split()
-cnt = 0
-for i, s in enumerate(line):
-    if s.isdigit():
-        cnt+= len(s)
-if cnt == 0:
-    print("Нема цифор")
-else:
-    print("", "от стока цифор", cnt)
+def Len (s):
+    if type (s) != str:
+        raise TypeError ('Expected str, got {}'.format(type(s)) )
+
+    return s[: : -1]
